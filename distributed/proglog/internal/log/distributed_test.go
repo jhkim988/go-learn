1dac8836a70f7c848f38a8888cd5657f46eeb4d9
jhkim988 <fccva3910@gmail.com>
2023-08-15 17:34:07 +0900

    distributed 테스트 코드

---
+++ distributed/proglog/internal/log/distributed_test.go
@@ -0,0 +1,107 @@
+package log_test
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	api "github.com/jhkim988/proglog/api/v1"
+	"github.com/jhkim988/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+)
+
+func TestMultipleNodes(t *testing.T) {
+	var logs []*log.DistributedLog
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+
+	for i := 0; i < nodeCount; i++ {
+		dataDir, err := os.MkdirTemp("", "distributed-log-test")
+		require.NoError(t, err)
+
+		defer func(dir string) {
+			_ = os.RemoveAll(dir)
+		}(dataDir)
+
+		ln, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", ports[i]))
+		require.NoError(t, err)
+
+		config := log.Config{}
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+
+		if i == 0 {
+			config.Raft.Bootstrap = true
+		}
+
+		l, err := log.NewDistritubedLog(dataDir, config)
+		require.NoError(t, err)
+
+		if i != 0 {
+			err = logs[0].Join(fmt.Sprintf("%d", i), ln.Addr().String())
+			require.NoError(t, err)
+		} else {
+			err = l.WaitForLeader(3 * time.Second)
+			require.NoError(t, err)
+		}
+		logs = append(logs, l)
+	}
+
+	records := []*api.Record{
+		{Value: []byte("first")},
+		{Value: []byte("second")},
+	}
+
+	for _, record := range records {
+		// 로그 추가
+		off, err := logs[0].Append(record)
+		require.NoError(t, err)
+
+		require.Eventually(t, func() bool {
+			// 로그 읽어서 모두 추가됐는지 확인
+			for j := 0; j < nodeCount; j++ {
+				got, err := logs[j].Read(off)
+				if err != nil {
+					return false
+				}
+				record.Offset = off
+				if !reflect.DeepEqual(got.Value, record.Value) {
+					return false
+				}
+			}
+			return true
+		}, 500*time.Millisecond, 50*time.Millisecond)
+	}
+
+	// 서버 제거
+	err := logs[0].Leave("1")
+	require.NoError(t, err)
+	time.Sleep(50 * time.Millisecond)
+
+	// 로그 추가
+	off, err := logs[0].Append(&api.Record{
+		Value: []byte("third"),
+	})
+	require.NoError(t, err)
+	time.Sleep(50 * time.Millisecond)
+
+	// 제거 후에 로그를 추가했으므로 읽을 수 없어야 한다.
+	record, err := logs[1].Read(off)
+	require.IsType(t, api.ErrOffsetOutOfRange{}, err)
+	require.Nil(t, record)
+
+	// 제거되지 않은 노드는 읽을 수 있다.
+	record, err = logs[2].Read(off)
+	require.NoError(t, err)
+	require.Equal(t, []byte("third"), record.Value)
+	require.Equal(t, off, record.Offset)
+}
